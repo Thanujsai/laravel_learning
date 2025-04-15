cbe06111a82cb829ed2695be37d77c515e285e38
thanujsai <sboda@modeln.com>
2025-04-15 16:14:26 +0530

    vanilla php

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,7 +1,17 @@
-@props(['active' => false]){{--this should be explicitly mentioned if we want to use it as a prop--}}
+@props(['active' => false, 'type' => 'a']){{--this should be explicitly mentioned if we want to use it as a prop--}}
+
+<?php if ($type === 'a') : ?>
 <a class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium" 
     aria-current="{{ $active ? 'page' : 'false'}}"
     {{ $attributes}}
 >
     {{ $slot }}
 </a>
+<?php else : ?>
+<button class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium" 
+    aria-current="{{ $active ? 'page' : 'false'}}"
+    {{ $attributes}}
+    >
+    {{ $slot }}
+</button>
+<?php endif; ?>
