624dd2cf7f41f72077ff4c697e2276898e8b5176
thanujsai <sboda@modeln.com>
2025-04-16 19:54:32 +0530

    sending an array prop to the view, displaying the array in the view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,8 @@
     <x-slot:heading>{{-- defining the variable/prop heading here in order to use it in layout --}}
         Home Page
     </x-slot:heading>
-    <h1>{{ $greeting }} from the welcome page,  {{$name}}</h1>{{-- I'll have access to this variable name from web.php --}}
+    {{-- <h1>{{ $greeting }} from the welcome page,  {{$name}}</h1>I'll have access to this variable name from web.php --}}
+    @foreach ($jobs as $job){{-- getting this jobs variable from the endpoint defined in web.php --}}
+        <li><b>Title</b> : {{$job['title']}}, <b>Salary</b> : {{$job['salary']}}</li>
+    @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome', [
-        'greeting' => 'Hellooo',
-        'name' => 'Thanuj'
+        'jobs' => [
+            [
+                'title' => 'Director',
+                'salary' => '$50,000',
+            ],
+            [
+                'title' => 'Programmer',
+                'salary' => '$10,000',
+            ],
+            [
+                'title' => 'Teacher',
+                'salary' => '$40,000',
+            ],
+        ]
     ]);//passing an array to the view
 });
 
