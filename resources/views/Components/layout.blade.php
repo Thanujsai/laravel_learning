76ee6628c71c7f720ae80572245fb6303bc0483b
thanujsai <sboda@modeln.com>
2025-04-16 22:35:06 +0530

    contact button enable

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -31,7 +31,7 @@
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <x-nav-link href="/" :active="request()->is('/')" id="home">Home</x-nav-link>
                 <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
-                <x-nav-link :active="request()->is('contact')" type="button">Contact</x-nav-link><!--prefixing a name with a colon means that the value should be interpreted as a value or expression, not as a string-->
+                <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link><!--prefixing a name with a colon means that the value should be interpreted as a value or expression, not as a string-->
               </div>
             </div>
           </div>
