8458b80c0410b155c9a7d93ebab2c9a93bad2ca5
thanujsai <sboda@modeln.com>
2025-04-18 16:33:45 +0530

    generate fake data, migrations etc

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,8 +4,10 @@ namespace App\Models;
 use Illuminate\Support\Arr;
 
 use Illuminate\Database\Eloquent\Model; //importing the Model class from the Eloquent ORM
+use Illuminate\Database\Eloquent\Factories\HasFactory; //importing the HasFactory trait from the Eloquent ORM
 
 class Job extends Model{ //to make a php class into the eloquent model it just need to extend the Model class
+    use HasFactory;//Hey, this model has an associated factory, and I might want to generate fake data using it.
     protected $table = "job_listings";//since the table name is not the same as the model name, we need to specify the table name, otherwise it's not necessary
 
     protected $fillable = ['title', 'salary'];//these are the fields that we want to be mass assignable, so we can use the create method to create a new job
--- database/migrations/2025_04_17_175640_create_job_listings_table.php
+++ database/migrations/2025_04_17_175640_create_job_listings_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
+            //foreign id called employer_id which says the job listing belongs to an employer
+            //$table->unsignedBigInteger('employer_id');//this is the foreign key that references the id of the employer, so we need to create a foreign key constraint
+            $table->foreignIdFor(App\Models\Employer::class);
             $table->string('title');//title and salary are the fields in our job model, it needs to be present in the table too, so we need to create the table with these fields
             $table->string('salary');
             $table->timestamps();
