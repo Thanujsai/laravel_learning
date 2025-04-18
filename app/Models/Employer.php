2cb3a0dcfb703fdcfd394b094388588e062640ac
thanujsai <sboda@modeln.com>
2025-04-18 23:30:22 +0530

    establishing relation between job and employer entities

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -9,4 +9,11 @@ class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    //An employer can take care of multiple jobs.
+    //this builds the relation between employer and jobs
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
