06f29b311262029ca99aac6c6f645e0a272acd2a
thanujsai <sboda@modeln.com>
2025-04-22 23:31:11 +0530

    eager loading, lazy loading, n+1 problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,9 +5,16 @@
 
     <div class="space-y-4">
         @foreach ($jobs as $job){{-- getting this jobs variable from the endpoint defined in web.php --}}
-            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded hover:text-blue-500">
-                <b>Title</b> : {{$job['title']}}, <b>Salary</b> : {{$job['salary']}}
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded hover:text-[#8D6FFE]">
+                <div class="font-bold text-blue-500 text-sm"><b>Employer</b> : {{$job->employer->name}}</div>
+                <b>Title</b> : {{$job['title']}}, <b>Salary</b> : {{$job['salary']}}<br></br>
             </a>
         @endforeach
     </div>
 </x-layout>
+
+<script>
+    console.log('jobs page')
+    console.log(@json($job))
+    console.log(@json($jobs)); // now includes employer details
+</script>
