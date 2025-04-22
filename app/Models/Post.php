99c86bf0a642ed2847ef2ebf8f2481fd42a36cdb
thanujsai <sboda@modeln.com>
2025-04-22 20:39:29 +0530

    small comment for understading

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,8 @@ class Post extends Model
     protected $table = 'posts'; // Specify the table name if it doesn't follow Laravel's naming convention
     protected $fillable = ['title', 'description']; // Specify the fillable fields for mass assignment
 
-    public function tags() {
+    public function tags() {//(belongsToMany means Many to Many, in this case, a post can have many tags, and a tag can belong to many posts)
         return $this->belongsToMany(Tag::class, foreignPivotKey: "post_id"); // Specify the foreign key if it's not the default
     } 
+
 }
